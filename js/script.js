91899a9fd540c75003e7f3e9ef81ed9c93f2615a
lopesnuno <lopesnuno@ipvc.pt>
2023-11-20 10:01:09 +0000

    Sticky navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
-// Add your custom JavaScript code here
-document.addEventListener("DOMContentLoaded", function() {
-    // Your JavaScript logic goes here
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  var homeSection = document.getElementById("home");
+  var navbar = document.getElementById("navbar");
+  var isNavbarSticky = false; // Keep track of the sticky state
+
+  function isSectionVisible() {
+    var rect = homeSection.getBoundingClientRect();
+    return rect.bottom > 0 && rect.top < window.innerHeight;
+  }
+
+  function toggleStickyNavbar() {
+    var shouldBeSticky = !isSectionVisible();
+
+    if (shouldBeSticky !== isNavbarSticky) {
+      isNavbarSticky = shouldBeSticky;
+
+      if (isNavbarSticky) {
+        navbar.classList.add("sticky-nav");
+      } else {
+        navbar.classList.remove("sticky-nav");
+      }
+    }
+  }
+
+  // Initial check in case the page is loaded at a scrolled position
+  toggleStickyNavbar();
+
+  // Listen for scroll events
+  window.addEventListener("scroll", toggleStickyNavbar);
 });
